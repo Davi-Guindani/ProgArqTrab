4100601818683b619773121f7def92f4b2c10230
daviguindani <davigpvieira@gmail.com>
2023-05-09 11:19:05 -0300

    tentando usar as estruturas que ele pediu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -9,56 +10,59 @@ int main()
     int cmd = 0;
     FILE *arq;
     string fname;
-    map<string, int> joao;
-    joao["oi"] = 10;
-    joao["ola"] = 8;
-    map<string, int>::iterator it = joao.begin();
-    while (it != joao.end())
+
+    vector<map<string, int>> joao;
+    map<string, int> nameToIndex;
+
+    while (cmd != 4)
     {
-        cout << it->first;
-        cout << it->second;
-        it++;
+        cout << "Digite 1 para ler um arquivo CSV" << endl
+             << "Digite 2 para acessar o relatorio de uma crianca" << endl
+             << "Digite 3 para acessar o relatorio de um quesito" << endl
+             << "Digite 4 para sair" << endl;
+        cin >> cmd;
+        switch (cmd)
+        {
+        case 1:
+            cout << "Digite o nome do arquivo csv sem a extensao: ";
+            cin >> fname;
+
+            arq = fopen((fname + ".csv").c_str(), "r");
+            if (arq == NULL)
+                cout << "erro abrindo o arquivo" << endl;
+            else
+            {
+                if (nameToIndex.count(fname))
+                    cout << "arquivo ja carregado" << endl;
+                else
+                {
+                    nameToIndex.insert(pair<string, int>("ola", nameToIndex.size()));
+                    joao.push_back(nameToIndex);
+                    vector<map<string, int>>::iterator it;
+                    it = joao.begin();
+                    auto it1 = it->begin();
+                    cout << it1->first << " " << it1->second << endl;
+                }
+                // map<string, int> reg;
+                // reg = ("Ola", 2);
+                // carregar as informações do csv no modelo
+            }
+
+            fclose(arq);
+            break;
+        case 2:
+            // relatar criança
+            break;
+        case 3:
+            // relatar quesito
+            break;
+        case 4:
+            // sair
+            break;
+
+        default:
+            break;
+        }
     }
-    
-
-    // while (cmd != 4)
-    // {
-    //     cout << "Digite 1 para ler um arquivo CSV" << endl
-    //          << "Digite 2 para acessar o relatorio de uma crianca" << endl
-    //          << "Digite 3 para acessar o relatorio de um quesito" << endl
-    //          << "Digite 4 para sair" << endl;
-    //     cin >> cmd;
-    //     switch (cmd)
-    //     {
-    //     case 1:
-    //         cout << "Digite o nome do arquivo csv sem a extensao: ";
-    //         cin >> fname;
-    //         fname += ".csv";
-    //         arq = fopen(fname.c_str(), "r");
-    //         if (arq == NULL)
-    //             cout << "erro abrindo o arquivo" << endl;
-    //         else
-    //         {
-    //             // map<string, int> reg;
-    //             // reg = ("Ola", 2);
-    //             // carregar as informações do csv no modelo
-    //         }
-
-    //         break;
-    //     case 2:
-    //         // relatar criança
-    //         break;
-    //     case 3:
-    //         // relatar quesito
-    //         break;
-    //     case 4:
-    //         // sair
-    //         fclose(arq);
-    //         break;
-
-    //     default:
-    //         break;
-    //     }
-    // }
     return 0;
 }
